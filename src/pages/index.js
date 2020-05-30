bdd07d2c6abf064d62a9e04d068cc0cd53925be9
agnescameron <agnesfcameron@gmail.com>
2020-05-29 20:24:31 -0400

    figuring out starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { RichText } from "prismic-reactjs"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export const query = graphql` 
+{
+  prismic {
+    allArticles {
+      edges {
+        node {
+          title
+          _meta {
+            tags
+            uid
+          }
+        }
+      }
+    }
+  }
+}`
 
-export default IndexPage
+export default function Home({ data }) {
+  const articles = data.prismic.allArticles.edges
+  if (!articles) return null
+  else return (
+    <Layout>
+      <div>
+        { 
+          articles.map( (article, index) => { 
+            return <h1 key={index}>{article.node.title[0].text}</h1>
+          })
+        }
+      </div>
+    </Layout>
+  )
+}
